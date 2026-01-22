fe7fadba8533d01a783bbe441daac1e63a327230
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-22 11:39:30 -0300

    feat: adicionado o model de Registro

---
+++ backend/src/models/Registro.js
@@ -0,0 +1,53 @@
+class Registro {
+    id;
+    data_execucao;
+    descricao;
+    equipe;
+    id_ponto;
+
+    constructor (id, data_execucao, descricao, equipe, id_ponto) {
+        this.id = id;
+        this.data_execucao = data_execucao;
+        this.descricao = descricao;
+        this.equipe = equipe;
+        this.id_ponto = id_ponto;
+    }
+
+    get_id () {
+        return this.id;
+    }
+
+    get_data_execucao () {
+        return this.data_execucao;
+    }
+
+    get_descricao () {
+        return this.descricao;
+    }
+
+    get_equipe () {
+        return this.equipe;
+    }
+
+    get_id_ponto () {
+        return this.id_ponto;
+    }
+
+    set_data_execucao (data_execucao) {
+        this.data_execucao = data_execucao;
+    }
+
+    set_descricao (descricao) {
+        this.descricao = descricao;
+    }
+
+    set_equipe (equipe) {
+        this.equipe = equipe;
+    }
+
+    set_id_ponto (id_ponto) {
+        this.id_ponto = id_ponto;
+    }
+}
+
+export default Registro
