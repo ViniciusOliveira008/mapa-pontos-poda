7a5bde172f1c8532405489d3ef6414f707c3daac
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-26 11:51:49 -0300

    feat: Criado repositório de pontos

---
+++ backend/src/repositories/PontoRepository.js
@@ -0,0 +1,52 @@
+class PontoRepository {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async listar() {
+    const [rows] = await this.db.execute("SELECT * FROM pontos");
+    return rows;
+  }
+
+  async criar(ponto) {
+    const [result] = await this.db.execute(
+      "INSERT INTO pontos (id, numero_oi, tipo_plano, latitude, longitude, barramento, servico, status_defeito) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        ponto.id,
+        ponto.numero_oi,
+        ponto.tipo_plano,
+        ponto.latitude,
+        ponto.longitude,
+        ponto.barramento,
+        ponto.servico,
+        ponto.status_defeito,
+      ],
+    );
+    return result.insertId;
+  }
+
+  async encontrarPorId(id) {
+    const [rows] = await this.db.execute("SELECT * FROM pontos WHERE id = ?", [
+      id,
+    ]);
+    return rows[0];
+  }
+
+  async atualizar(id, ponto) {
+    await this.db.execute(
+      "UPDATE pontos SET numero_oi = ?, tipo_plano = ?, latitude = ?, longitude = ?, barramento = ?, servico = ?, status_defeito = ? WHERE id = ?",
+      [
+        ponto.numero_oi,
+        ponto.tipo_plano,
+        ponto.latitude,
+        ponto.longitude,
+        ponto.barramento,
+        ponto.servico,
+        ponto.status_defeito,
+        id,
+      ],
+    );
+  }
+}
+
+export default PontoRepository;
